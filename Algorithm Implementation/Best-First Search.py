594fe14765cb405c6f1eff245b8b9f4f9a540cc5
Rafeza Islam <rafajaislam120@gmail.com>
2025-10-28 22:07:44 +0600

    Add Best-First Search algorithm implementation
    Implement Best-First Search algorithm with user input for graph and heuristic values.

--- Algorithm Implementation/Best-First Search.py
+++ Algorithm Implementation/Best-First Search.py
@@ -1,1 +1,48 @@
+def best_first_search(graph, start, goal, h):
+    visited = []
+    frontier = [start]
+
+    while frontier:
+        
+        def heuristic_value(node):
+            return h[node]
+
+        frontier.sort(key=heuristic_value,reverse=False)
+
+        current = frontier.pop(0)
+        print("Visiting:", current)
+
+        if current == goal:
+            print("Goal reached:", goal)
+            return
+
+        if current not in visited:
+            visited.append(current)
+            for neighbor in graph[current]:
+                if neighbor not in visited and neighbor not in frontier:
+                    frontier.append(neighbor)
+
+    print("Goal not found!")
+
+
+def main():
+
+    n = int(input("Number of nodes: "))
+    graph = {}
+    h = {}
+
+    
+    for _ in range(n):
+        node = input("Node name: ")
+        neighbors = input(f"Neighbors of {node}: ").split()
+        graph[node] = neighbors 
+        h[node] = int(input(f"Heuristic value of {node}: "))
+
+    start = input("Start node: ")
+    goal = input("Goal node: ")
+    best_first_search(graph, start, goal, h)
+if __name__=="__main__":
+    main()
+
+
 
